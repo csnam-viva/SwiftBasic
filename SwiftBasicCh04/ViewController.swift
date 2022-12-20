45f132aeb814dc4d7838bbe00c66bc2fba803e4e
csnam-apple <dontforgetcosmos@gmail.com>
2022-12-20 20:43:56 +0900

    Initial Commit

---
+++ SwiftBasicCh04/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftBasicCh04
+//
+//  Created by 비바 on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func moveNext(_ sender: Any) {
+        let uvc = self.storyboard!.instantiateViewController(withIdentifier: "SecondVC")
+        uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        self.present(uvc, animated: true)
+    }
+}
+
